121eefc734988d9ce785dbdc8e4ef282aaa8ddd0
Daniel Coley <dmcoley948562@gmail.com>
2016-12-13 13:42:38 -0700

    rails update

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  
+
+  get "contact", to: "pages#contact"
+
+  get "about", to: "pages#about"
+  
+  get "error", to: "pages#error"
+
+  get "blog", to: redirect("http://jordanhudgens.com")
+
+  resources :projects do
+    resources :tasks, except: [:index], controller: 'projects/tasks'
+  end
+
+  root 'pages#home'
+  
+  get "*path", to: redirect("/error")
+end
